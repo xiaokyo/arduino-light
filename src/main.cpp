f8451d8e452e72a58be1b12c45ca077ae37ac886
小K <904656778@qq.com>
2023-02-06 19:55:40 +0800

    代码优化

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,15 @@ char ssid[] = "9-1-10";
 // wifi 密码
 char pswd[] = "qw123123";
 
+const LIGHT = D2;
+
 // 新建组件对象
-BlinkerButton Button_open_light((char *)"open-light");
-BlinkerButton Button_close_light((char *)"close-light");
-
-/**
- * 开灯
- */
-void openLight()
-{
-  digitalWrite(D2, HIGH);
-}
-
-/**
- * 关灯
- */
-void closeLight()
-{
-  digitalWrite(D2, LOW);
-}
+BlinkerButton button_switch_light((char *)"open-light");
 
 // 按下按键即会执行该函数
-void button_open_light_handle(const String &state)
+void button_switch_light_handle(const String &state)
 {
-  BLINKER_LOG("get button state: ", state);
-  openLight();
-}
-
-void button_close_light_handle(const String &state)
-{
-  BLINKER_LOG("get button state: ", state);
-  closeLight();
+  digitalWrite(LIGHT, !digitalRead(LIGHT));
 }
 
 void setup()
@@ -46,13 +24,12 @@ void setup()
   Serial.begin(115200);
 
   // 初始化
-  pinMode(D2, OUTPUT);
-  digitalWrite(D2, LOW);
+  pinMode(LIGHT, OUTPUT);
+  digitalWrite(LIGHT, LOW);
 
   // 初始化blinker
   Blinker.begin(auth, ssid, pswd);
-  Button_open_light.attach(button_open_light_handle);
-  Button_close_light.attach(button_close_light_handle);
+  button_switch_light.attach(button_switch_light_handle);
 }
 
 void loop()
