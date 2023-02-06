52c03cdf7fd3e664b916dcfcabe3bef5499e2570
小K <904656778@qq.com>
2023-02-06 11:59:11 +0800

    初始提交

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "wrapper_header.h"
+
+char auth[] = "c1d3d29e7cc4";
+char ssid[] = "9-1-10";
+char pswd[] = "qw123123";
+
+// 新建组件对象
+BlinkerButton Button1((char *)"btn-abc");
+BlinkerButton Button2((char *)"btn-cba");
+BlinkerNumber Number1((char *)"num-abc");
+
+/**
+ * 开灯
+*/
+void openLight()
+{
+  digitalWrite(D2, HIGH);
+}
+
+/** 
+ * 关灯
+*/
+void closeLight()
+{
+  digitalWrite(D2, LOW);
+}
+
+// 按下按键即会执行该函数
+void button1_callback(const String &state)
+{
+  BLINKER_LOG("get button state: ", state);
+  openLight();
+}
+
+void button2_callback(const String &state)
+{
+  BLINKER_LOG("get button state: ", state);
+  closeLight();
+}
+
+void setup()
+{
+  // 初始化串口
+  Serial.begin(115200);
+
+  pinMode(D2, OUTPUT);
+  digitalWrite(D2, LOW);
+
+  // 初始化blinker
+  Blinker.begin(auth, ssid, pswd);
+  Button1.attach(button1_callback);
+  Button2.attach(button2_callback);
+}
+
+void loop()
+{
+  Blinker.run();
+}
---
+++ src/wrapper_header.h
@@ -0,0 +1,9 @@
+#pragma GCC system_header
+
+#define BLINKER_WIFI
+
+#include <Blinker.h>
+
+/**
+ * 这个头文件是为了忽略第三方warning
+*/
